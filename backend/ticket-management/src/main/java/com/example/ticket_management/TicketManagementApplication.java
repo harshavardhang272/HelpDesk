e12198422ef571ca4af5bb015eff2114c485cd48
Harshak-1744 <harshavardhan.hv36@gmail.com>
2024-07-10 21:10:34 +0530

    modified backend

---
+++ backend/ticket-management/src/main/java/com/example/ticket_management/TicketManagementApplication.java
@@ -0,0 +1,13 @@
+package com.example.ticket_management;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicketManagementApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TicketManagementApplication.class, args);
+	}
+
+}
